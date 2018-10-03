58d9ac7ca2b7bfdf3f58da5ebd6431da46bfee2b
Julien Seiler <seilerj@igbmc.fr>
2018-10-03 22:21:16 +0200

    Fix meta data and build script to comply with version 4.1.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,15 +8,14 @@ if [ "$(uname)" == "Darwin" ]; then
 	export INSTALL_ROOT=${PREFIX}
 	./configure --prefix=$PREFIX  --with-tcl=$PREFIX/lib
 	make prefix=$PREFIX
-	make prefix=$PREFIX install 
-
-        INIT=${PREFIX}/init/bash
+	make prefix=$PREFIX install
+  INIT=${PREFIX}/init/bash
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib
+	./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib --with-tclsh=$PREFIX/bin/tclsh
 	make
 	make install
-	INIT=${PREFIX}/Modules/3.2.10/init/bash
+	INIT=${PREFIX}/init/bash
 fi
 
 mkdir -p $PREFIX/etc/conda/activate.d/
